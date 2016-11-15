9bda02f35ec81630c030f7975bab694614c5b873
JeanTroiani <digitalguest@gmail.com>
2016-11-15 07:53:09 +0000

    Add files via upload
    Extracted view component of character

---
+++ CharacterItem/CharacterItem.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var React = require('react');
+
+var CharacterItem =  React.createClass({
+    render: function() {
+        return (
+            <section>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Name:</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.props.characters.map(function(character, id){
+                                return(
+                                    <tr key={id}>
+                                        <td>{character}</td>
+                                    </tr>
+                                );
+                            },this )}
+                        </tbody>
+                    </table>
+            </section>
+        );
+    }
+});
+
+export default CharacterItem;
